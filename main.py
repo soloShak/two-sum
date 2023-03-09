0a6de6f7f78ed6924abc280894c2a371f3aa0aa6
Ubuntu <shakzod02@OSSP.g552eadw4twuldzpdhrshiuyfb.hx.internal.cloudapp.net>
2023-03-09 11:29:37 +0000

    Second commit, The final version of a program

--- main.py
+++ main.py
@@ -12,7 +12,3 @@ def twoSum(nums, target):
 		"""for j in range(i, len(nums)):
 			if nums[i] + nums[j] == target:
 				return [i, j]"""
-nums =[3,2,4]
-target = 6
-answer = twoSum(nums, target)
-print(answer)
